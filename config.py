b55436d4ad6aa2f9932d356189eb53d1ee850f56
Nikhil1-234 <142384405+Nikhil1-234@users.noreply.github.com>
2023-08-18 02:23:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,6 @@ API_ID = int(environ.get("API_ID", "22299340"))
 API_HASH = environ.get("API_HASH", "09b09f3e2ff1306da4a19888f614d937")
 BOT_TOKEN = environ.get("BOT_TOKEN", "6446622723:AAHcsDFRYj_s45VgvBCakSOUXeCN5e2O42w")
 ADMIN = int(environ.get("ADMIN", "5380609667"))          
-CAPTION = environ.get("CAPTION", "@animelibraryn4")
-
-# for thumbnail ( back end is MrMKN brain 😉)
+CAPTION = environ.get("CAPTION", "("")
 DOWNLOAD_LOCATION = "./DOWNLOADS"
 
